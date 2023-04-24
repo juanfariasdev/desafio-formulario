1e396afb67c7d542be19056e36183a71b78ddb22
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2023-04-24 17:44:50 -0300

    added fields

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -2,8 +2,15 @@ import { InputLabel } from "./input"
 
 function Form(){
     return(
-    <form>
-        <InputLabel/>
+    <form className="flex flex-col gap-3">
+        <div className="flex gap-3">
+            <InputLabel placeholder="Nome" />
+            <InputLabel placeholder="Sobrenome" />
+        </div>
+        <InputLabel placeholder="Email" />
+        <InputLabel placeholder="CPF" />
+        <InputLabel placeholder="Senha" />
+        <InputLabel placeholder="Confirmar Senha" />
     </form>
     )
 }
