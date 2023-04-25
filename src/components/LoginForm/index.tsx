d28c3001a292ada4eedf83a788331e05852f6d55
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2023-04-25 16:57:56 -0300

    verify fields from form login

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,10 +1,30 @@
+"use client";
+import { useForm } from "react-hook-form";
 import { InputLabel } from "../input";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { object, string } from "yup";
 
+
+const msgRequired = "Campo obrigatório";
+const schema = object({
+  email: string().required(msgRequired).email("Email inválido").min(6, 'Deve conter pelo menos 6 dígitos'),
+  password: string().required(msgRequired)
+});
 function LoginForm() {
+  const {
+    handleSubmit: onSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const handleSubmit = (data: any) => {
+    console.log(data);
+  };
   return (
-    <form className="flex flex-col gap-3">
-      <InputLabel placeholder="Email" />
-      <InputLabel placeholder="Senha" type="password" />
+    <form className="flex flex-col gap-3" onSubmit={onSubmit(handleSubmit)}>
+      <InputLabel placeholder="Email"id="email" register={register} error={errors?.email?.message}/>
+      <InputLabel placeholder="Senha" type="password" id="password" register={register} error={errors?.password?.message}/>
       <button className="rounded bg-yellow-500 hover:bg-yellow-600 text-xl p-3 text-white w-full transition font-bold">
         Login
       </button>
