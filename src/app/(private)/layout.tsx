7e59e6c091d83b73d8321a2d8f58ede6ab93b231
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2023-05-02 09:22:22 -0300

    fix duplicate information layout

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,16 +1,6 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-
-import "../globals.css";
 
 import { Menu } from "@/components/menu";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Academia do Aluno",
-  description: "Criado para facilitar o encontro de emprego",
-};
 
 export default function DashboardLayout({
   children,
@@ -18,36 +8,30 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-br">
-      <body className={inter.className}>
-          <div className="flex min-h-screen justify-between flex-col lg:flex-row bg-zinc-900 p-4">
-          <aside className="text-gray-400 font-bold flex-col w-72  flex py-10 pr-6">
-            {/* Perfil */}
-            <div className="flex gap-2 items-center w-full pb-3 mb-3 border-b border-gray-500">
-              <Image
-                src={"https://github.com/juanfariasdev.png"}
-                alt={""}
-                className="rounded-full w-16"
-                width={75}
-                height={75}
-              />
-              <div className="flex flex-col ">
-                <h3 className="text-white text-lg overflow-hidden text-ellipsis whitespace-nowrap max-w-[20ch]">
-                  Juan Pablo Farias
-                </h3>
-                <span className="text-lg font-normal">Administrador</span>
-              </div>
-            </div>
-            {/* Menu */}
-            <nav className="text-xl">
-              <Menu />
-            </nav>
-          </aside>
-            <main className="bg-white flex-1 rounded-3xl px-8 py-6">
-            {children}
-          </main>
+    <div className="flex min-h-screen justify-between flex-col lg:flex-row bg-zinc-900 p-4">
+      <aside className="text-gray-400 font-bold flex-col w-72  flex py-10 pr-6">
+        {/* Perfil */}
+        <div className="flex gap-2 items-center w-full pb-3 mb-3 border-b border-gray-500">
+          <Image
+            src={"https://github.com/juanfariasdev.png"}
+            alt={""}
+            className="rounded-full w-16"
+            width={75}
+            height={75}
+          />
+          <div className="flex flex-col ">
+            <h3 className="text-white text-lg overflow-hidden text-ellipsis whitespace-nowrap max-w-[20ch]">
+              Juan Pablo Farias
+            </h3>
+            <span className="text-lg font-normal">Administrador</span>
+          </div>
         </div>
-      </body>
-    </html>
+        {/* Menu */}
+        <nav className="text-xl">
+          <Menu />
+        </nav>
+      </aside>
+      <main className="bg-white flex-1 rounded-3xl px-8 py-6">{children}</main>
+    </div>
   );
 }
--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,16 +1,5 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-
 import Hero from "../../assets/hero.jpg";
-
-import "../globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Academia do Aluno",
-  description: "Criado para facilitar o encontro de emprego",
-};
 
 export default function RootLayout({
   children,
@@ -18,25 +7,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-br">
-      <body className={inter.className}>
-        <div className="flex min-h-screen justify-between flex-col lg:flex-row">
-          <main className="bg-zinc-900 text-white min-h-[500px] lg:max-w-xl w-full rounded-t-2xl lg:rounded-t-none lg:rounded-r-2xl flex flex-col justify-center p-10 lg:px-20 order-2 lg:order-1">
-            {children}
-          </main>
-          <aside className="flex-col flex-1 justify-center items-center flex order-1 py-10">
-            <Image
-              src={Hero}
-              alt=""
-              className="w-auto lg:w-80 max-h-[30vh] lg:max-h-full"
-              width={500}
-            />
-            <h1 className="text-3xl font-bold">Seja muito bem-vindo!</h1>
-            <p className="text-xl">Veio melhorar suas habilidades?</p>
-            <p className="text-xl">Vamos te ajudar =)</p>
-          </aside>
-        </div>
-      </body>
-    </html>
+    <div className="flex min-h-screen justify-between flex-col lg:flex-row">
+      <main className="bg-zinc-900 text-white min-h-[500px] lg:max-w-xl w-full rounded-t-2xl lg:rounded-t-none lg:rounded-r-2xl flex flex-col justify-center p-10 lg:px-20 order-2 lg:order-1">
+        {children}
+      </main>
+      <aside className="flex-col flex-1 justify-center items-center flex order-1 py-10">
+        <Image
+          src={Hero}
+          alt=""
+          className="w-auto lg:w-80 max-h-[30vh] lg:max-h-full"
+          width={500}
+        />
+        <h1 className="text-3xl font-bold">Seja muito bem-vindo!</h1>
+        <p className="text-xl">Veio melhorar suas habilidades?</p>
+        <p className="text-xl">Vamos te ajudar =)</p>
+      </aside>
+    </div>
   );
 }
