7c84caa6b45b25e0afe1e62715875a95fc67667a
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2023-05-03 08:17:47 -0300

    modified authOptions location

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,7 @@
 import { LoginForm } from "@/components/LoginForm";
+import { authOptions } from "@/config/nextAuth/authOptions";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+
 import { redirect } from "next/navigation";
 
 export default async function Home() {
---
+++ src/config/nextAuth/authOptions.ts
@@ -0,0 +1,15 @@
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: "/dashbaord",
+    signOut: "/",
+  },
+};
